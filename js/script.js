7e40de420585eb243efe9c85f29b19b9780acc59
lassethorlepik <lassethorlepik@gmail.com>
2021-12-29 21:27:26 +0200

    test

--- js/script.js
+++ js/script.js
@@ -1,19 +1,5 @@
-function videotap() {
-	fullscreen();
+function clickplay() {
+	document.getElementById("lumi").style.display = "none";
 	var myVideo = document.getElementById("video");
-	if (myVideo.paused) {
-		myVideo.play();
-		document.getElementById("lumi").style.display = "none";
-		document.getElementById("video").classList.add("hide");
-	}
-	else  {
-		myVideo.pause();
-		document.getElementById("video").classList.remove("hide");
-	}
+	document.getElementById('video').play();
 }
-$(document).on('tap click', function(){videotap()});
-
-function fullscreen() {
-
-}
-
